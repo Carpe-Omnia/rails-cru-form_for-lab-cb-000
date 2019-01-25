30fa9c77e9ad5e61ded901a2adbebee373756df5
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-25 19:18:04 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,10 @@
 class SongsController < ApplicationController
   def index
-    @songs = song.all
+    @songs = Song.all
   end
 
   def create
-    @song = song.new(song_params(:name))
+    @song = Song.new(song_params(:name))
     art = Artist.find_or_create_by(name: params[:song][:artist])
     gen = Genre.find_or_create_by(name: params[:song][:genre])
     @song.artist_id = art.id
@@ -17,17 +17,17 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = song.find_by(id: params[:id])
+    @song = Song.find_by(id: params[:id])
   end
 
   def update
-    @song = song.find(params[:id])
+    @song = Song.find(params[:id])
     @song.update(song_params(:name))
     redirect_to song_path(@song)
   end
 
   def edit
-    @song = song.find(params[:id])
+    @song = Song.find(params[:id])
   end
 
   private
