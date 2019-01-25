fdc93f3d46040b1066ebfe57d440fa73cc7aea74
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-25 18:16:02 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,9 @@
 class ArtistsController < ApplicationController
   def index
-    @Artists = Artist.all 
-  end 
-  def show 
+    @Artists = Artist.all
+  end
+  def show
     @artist = Artist.find_by(id: params[:id])
-  end 
-      
+  end
+
 end
