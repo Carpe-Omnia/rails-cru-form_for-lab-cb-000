a221afaf4571ec436d954e76c0e158400616e7ad
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-25 18:49:29 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,33 @@
 class GenresController < ApplicationController
+  def index
+    @genres = genre.all
+  end
+
+  def create
+    @genre = genre.new(genre_params(:name, :bio))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def new
+  end
+
+  def show
+    @genre = genre.find_by(id: params[:id])
+  end
+
+  def update
+    @genre = genre.find(params[:id])
+    @genre.update(genre_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = genre.find(params[:id])
+  end
+
+  private
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
 end
