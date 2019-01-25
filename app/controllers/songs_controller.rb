e8bb27b49be913c93521693af5ea76e3da1ec958
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-25 18:50:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,33 @@
 class SongsController < ApplicationController
+  def index
+    @songs = song.all
+  end
+
+  def create
+    @song = song.new(song_params(:name, :artist))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  def new
+  end
+
+  def show
+    @song = song.find_by(id: params[:id])
+  end
+
+  def update
+    @song = song.find(params[:id])
+    @song.update(song_params(:name, :bio))
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = song.find(params[:id])
+  end
+
+  private
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
 end
