1666ff8a0b779f34ace90673b621f4752565692a
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-25 18:27:36 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,7 +7,7 @@ class ArtistsController < ApplicationController
   def create
     @artist = Artist.new(artist_params(:name, :bio))
     @artist.save
-    redirect_to artists_path(@artist)
+    redirect_to artist_path(@artist)
   end
   def show
     @artist = Artist.find_by(id: params[:id])
